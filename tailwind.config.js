b9f17b9c05c6fa29659c95320fd2efb5a1bc7b72
ftoom <fujitom661@gmail.com>
2022-03-29 14:45:46 +0900

    mv fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,6 @@ module.exports = {
     extend: {
       backgroundImage: {
         'profile': "url('../../public/profile.png')",
-        'mv-0': "url('../../public/mv0.jpg')",
-        'mv-1': "url('../../public/mv1.jpg')",
       },
       fontSize: {
         'xxs': '.625rem'
