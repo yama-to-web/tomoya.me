e97d63dc3bfd42d2d747bd504022a9437683b25c
ftoom <fujitom661@gmail.com>
2022-06-05 22:41:17 +0900

    Product追加

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -39,18 +39,40 @@ const About: NextPage = () => {
             </div>
           </div>
         </Section>
+        <Section title="Works">Comming Soon...</Section>
         <Section title="Skills">
-          <div className="px-2 lg:max-w-screen-lg">
-            <p className="text-lg font-thin">Certification</p>
-            <ul>
-              <li className="p-2 border-b-2">
+          <div className="mb-8 lg:max-w-screen-lg">
+            <p className="mb-4 w-fit text-xl font-thin">Products</p>
+            <ul className="pl-1">
+              <li className="py-2 border-b-2 border-b-gray-100">
+                <p className="text-xxs text-gray-500">Webサイト</p>
+                <p className="mb-3 text-xl leading-4">
+                  tomoya.me
+                  <span className="pl-1 text-xs">- ポートフォリオ</span>
+                </p>
+                <ul className="flex flex-wrap gap-1 text-xs font-semibold text-slate-500">
+                  <li className="px-1 before:content-['#'] bg-slate-200 rounded-lg">Next.js</li>
+                  <li className="px-1 before:content-['#'] bg-slate-200 rounded-lg">React</li>
+                  <li className="px-1 before:content-['#'] bg-slate-200 rounded-lg">TypeScript</li>
+                  <li className="px-1 before:content-['#'] bg-slate-200 rounded-lg">
+                    Tailwind CSS
+                  </li>
+                  <li className="px-1 before:content-['#'] bg-slate-200 rounded-lg">Figma</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div className="mb-8 lg:max-w-screen-lg">
+            <p className="mb-4 w-fit text-xl font-thin">Certification</p>
+            <ul className="pl-1">
+              <li className="py-2 border-b-2 border-b-gray-100">
                 <p className="text-xs text-gray-500">2022.05.23</p>
                 <p className="text-sm leading-6">
                   Associate Cloud Engineer
                   <span className="pl-1 text-xxs">- Google Cloud Certified</span>
                 </p>
               </li>
-              <li className="p-2 border-b-2">
+              <li className="py-2 border-b-2 border-b-gray-100">
                 <p className="text-xs text-gray-500">2021.04.27</p>
                 <p className="text-sm leading-6">
                   基本情報技術者<span className="pl-1 text-xxs">- 情報処理推進機構(IPA)</span>
